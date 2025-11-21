29b2d855df44b58f34c4c63e1ee824d89093da98
Trung Thái <trungthai230700@gmail.com>
2025-11-21 16:09:55 +0700

    build prop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import './App.css'
-import Courses from './Courses'
+import Product from './Product'
 
 function App() {
-  return (
-    <>
-      <Courses />
-    </>
+  const listProduct = [
+    {
+      url: 'https://down-vn.img.susercontent.com/file/vn-50009109-7e80ab64bdc989f5c0862ed828f343a2',
+      title: 'Ưu đãi đến 50%',
+      price: '1999',
+    },
+    {
+      url: 'https://down-vn.img.susercontent.com/file/vn-50009109-7e80ab64bdc989f5c0862ed828f343a2',
+      title: 'Ưu đãi đến 50%',
+      price: '1999',
+    },
+    {
+      url: 'https://down-vn.img.susercontent.com/file/vn-50009109-7e80ab64bdc989f5c0862ed828f343a2',
+      title: 'Ưu đãi đến 50%',
+      price: '1999',
+    },
+    {
+      url: 'https://down-vn.img.susercontent.com/file/vn-50009109-7e80ab64bdc989f5c0862ed828f343a2',
+      title: 'Ưu đãi đến 50%',
+      price: '1999',
+    },
+    {
+      url: 'https://down-vn.img.susercontent.com/file/vn-50009109-7e80ab64bdc989f5c0862ed828f343a2',
+      title: 'Ưu đãi đến 50%',
+      price: '1999',
+    },
+  ];
+
+  return (  
+    <div style={{display: 'flex', gap:'20px'
+    }}>
+    {
+      listProduct.map((product, index) => {
+        return <Product key={index} title={product.title} url={product.url}/>
+      }) 
+    }
+      
+    </div>
   );
 }
 
---
+++ src/Product.jsx
@@ -0,0 +1,10 @@
+export default function Product({title, url, price})   {
+
+
+    console.log(title,url,price)
+    return <div>
+        <img src={url} style={{width: '183px', height: '183px'}} />
+        <div>{title}</div>
+        <div>Từ {price}</div>
+    </div>;
+}
